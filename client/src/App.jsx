33195a3d9f095c919743c1deb19be2b1701f3974
Mahmud <mahmud.linked@gmail.com>
2023-10-07 01:20:47 +0600

    added react-router-dom and used it to create route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import SignOut from './pages/SignUp'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+
 
 const App = () => {
   return (
-    <h1 className="text-red-900 font-bold">Hellow!</h1>
+    <BrowserRouter>  
+      <Routes>
+        <Route path='/' element = { <Home />}></Route>
+        <Route path='/about' element = { <About />}></Route>
+        <Route path='/sign-in' element = { <Signin />}></Route>
+        <Route path='/sign-up' element = { <SignUp />}></Route>
+        <Route path='/profile' element = { <Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
