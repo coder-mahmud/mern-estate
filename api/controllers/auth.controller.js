963e0ccf501a958e2abeebcfb1e599d478fe9772
Mahmud <mahmud.linked@gmail.com>
2023-10-07 22:01:44 +0600

    Added custom error handler on api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,13 @@
 import User from "../models/user.model.js";
 
 import bcrypt from 'bcryptjs'
+import { errorHandler } from "../utils/Error.js";
 
-export const authController  = async(req,res) =>{
+export const authController  = async(req,res, next) =>{
   const {username, email, password} = req.body;
   //console.log(username, email, password);
+
+  /*
   const foundUser = await User.findOne ({ "username" : username });
   if(foundUser){
     res.status(200).json({"message":"Username already taken"})
@@ -16,12 +19,14 @@ export const authController  = async(req,res) =>{
     res.status(200).json({"message":"email aready used!"})
     return;
   }
+
+  */
   const hashedPass = bcrypt.hashSync(password, 10);
   const newUser = new User({username, email, password:hashedPass});
   await newUser.save()
   .then(result =>{
     res.status(200).json({"message":"Auth route!", data: result})
-  }).catch((error)=>console.log('Error in creating auth', error))
+  }).catch((error) => next(errorHandler(error.status, error.message)))
   ;
   //res.status(200).json({"message":"Auth route!"})
 }
--- api/index.js
+++ api/index.js
@@ -23,6 +23,19 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 
 
+app.use((err,req,res,next) =>{
+  const statuscode = err.statuscode || 500;
+  const message = err.message || "Internal server error!";
+
+  return res.status(statuscode).json({
+    success:false,
+    message,
+    statuscode
+  });
+
+
+});
+
 
 app.listen(3000, () =>{
   console.log('Server is running!!!!')
